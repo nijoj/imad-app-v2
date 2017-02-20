b5257345a961e722e1f15a2f56b634301acd5e82
nijoj <nijo.email@gmail.com>
2017-02-20 23:18:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,6 +9,6 @@ function moveRight(){
     marginLeft=marginLeft+5;
     image.style.marginLeft=marginLeft+'px';
 }
-image.onClick=function(){
+image.onclick=function(){
     var interval=setInterval(moveRight,50)
 };
