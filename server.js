3878813b542bb02f46fbee0442d405bd08bc8e6d
nijoj <nijo.email@gmail.com>
2017-02-20 22:40:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -66,7 +66,9 @@ app.get('/:articleName', function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
+app.get('/ui/main.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
