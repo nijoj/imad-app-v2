c32fca6b27508e0b34612ccccf67bb84eddd8d3a
nijoj <nijo.email@gmail.com>
2017-03-13 20:43:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -63,6 +63,12 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });   
 
+//counter endpoint
+var counter=0;
+app.get('/counter',function(req,res){
+    counter++;
+    res.send(counter.toString());
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
