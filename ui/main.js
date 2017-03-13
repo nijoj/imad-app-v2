461b604e75aceea22433f712611f19479b9064a8
nijoj <nijo.email@gmail.com>
2017-03-13 21:08:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-console.log('Loaded!');
+/*console.log('Loaded!');
 //change html
 var element = document.getElementById('maintext');
 element.innerHTML = 'New text';
@@ -11,4 +11,14 @@ function moveRight(){
 }
 image.onclick=function(){
     var interval=setInterval(moveRight,50)
-};
+};*/
+
+//counter endpoint
+
+var button=document.getElementById("counter");
+var counter=0;
+button.onClick=function(){
+    counter++;
+    var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+}
