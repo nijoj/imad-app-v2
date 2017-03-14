52bb16cb6a171277c2e69b94546458a0b5f6889d
nijoj <nijo.email@gmail.com>
2017-03-14 19:20:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,7 +38,7 @@ var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
     var names=['name1','name2','name3'];
     var list='';
-    for(i=0;i<names.length;i++){
+    for(var i=0;i<names.length;i++){
         list+='<li>'+names[i]+'</li>';
     }
     val ul=document.getElementById('namelist');
