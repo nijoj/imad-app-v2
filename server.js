ca3df91d3406d0c6b68400b703ed6cae459dd5a5
nijoj <nijo.email@gmail.com>
2017-03-19 00:01:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -69,6 +69,15 @@ var htmlTemplate=`
     </html>`;
     return htmlTemplate;
 }
+//hashing
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return ['pbkdf2','10000',salt,hashed.toString('hex')].join('$');
+}
+app.get('/hash/:input',function(req,res){
+    var hashedString=hash(req.params.input,'salt');
+    res.send(hashedString);
+});
 //user create(p11)
 app.get('/create-user',function(req,res){
     var username=req.body.username;
@@ -84,16 +93,6 @@ app.get('/create-user',function(req,res){
         }
     });
 });
-//hashing
-function hash(input,salt){
-    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return ['pbkdf2','10000',salt,hashed.toString('hex')].join('$');
-}
-app.get('/hash/:input',function(req,res){
-    var hashedString=hash(req.params.input,'salt');
-    res.send(hashedString);
-});
-
 //database
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
