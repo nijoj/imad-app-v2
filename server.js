484b92769e855285f578b28d948a7b3971e4bd6f
nijoj <nijo.email@gmail.com>
2017-03-18 14:21:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/submit_name',function(req,res){
     names.push(name);
     res.send(JSON.stringify(names));
 });
-app.get('/articles/:articleName', function (req, res) {
+app.get('articles/:articleName', function (req, res) {
     pool.query("SELECT * FROM article WHERE title= '"+req.params.articleName+ "'", function(err,result){
         if (err){
             res.status(500).send(err.toString());
