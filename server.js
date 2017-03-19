f617089aa37267e97f3077ffaad07615caf3b8e0
nijoj <nijo.email@gmail.com>
2017-03-19 11:25:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var Pool = require('pg').Pool;
 var crypto=require('crypto');
 var bodyParser=require('body-parser');
+var session=require('express-session');
 var config = {
   host: 'db.imad.hasura-app.io',
   user: 'nijoj',
@@ -15,6 +16,10 @@ var config = {
 var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
+app.use(session({
+    secret:'random value';
+    cookie:{maxAge:1000*60*60*24*30}
+}));
 
 /*var articles={
      'pageone':{
@@ -110,6 +115,7 @@ app.post('/login',function(req,res){
                 var salt=dbString.split('$')[2];
                 var hashedPassword=hash(password,salt);
                 if(hashedPassword===dbString){
+                    req.session.auth={userId:result.rows[0].id};
                     res.send('credentials correct');
                 }
                 else{
@@ -118,6 +124,19 @@ app.post('/login',function(req,res){
             }
         }
     });
+});
+//check login
+app.get('/check-login',function(req,res){
+   if(req.session && req.session.auth && req.session.auth.userId){
+       res.send('You are logged in:',+req.session.auth.userId.toString());
+   } else{
+       res.send('You are not logged in');
+   }
+});
+//logout
+app.get('/logout',function(req,res){
+    delete.req.session.auth;
+    res.send('logged out');
 });
 //database
 var pool = new Pool(config);
