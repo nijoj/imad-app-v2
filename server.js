6dce8cbafbc1d8497800ac629d99e6bebdca994f
nijoj <nijo.email@gmail.com>
2017-03-19 11:54:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var app = express();
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(session({
-    secret:'random value';
+    secret:'random value',
     cookie:{maxAge:1000*60*60*24*30}
 }));
 
